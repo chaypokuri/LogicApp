7df757c0a8abe3eabdf1e5a45632760d1aa5dbfb
chaypokuri <61907472+chaypokuri@users.noreply.github.com>
2024-09-12 18:33:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "azurerm_logic_app_standard" "example" {
   name                       = "test-azure-functions"
   resource_group_name = "test-rg"
   location            = "east us"
-  storage_account_name       = "test-storage"
+  storage_account_name       = "Test-StorageAccount001"
   storage_account_access_key = "BjYCZEHX2OCqz/HWJdWp4rHfpLIAMJt0adLxzQnpItXCSyKwArG8iCgFfVNW/6pgabrtlniTcEaK+AStF5N8xQ=="
   app_service_plan_id     = "/subscriptions/c2bd123a-183f-43d5-bf41-c725494e595a/resourceGroups/test-rg/providers/Microsoft.Web/serverfarms/test-asp"
   virtual_network_subnet_id = "/subscriptions/c2bd123a-183f-43d5-bf41-c725494e595a/resourceGroups/test-rg/providers/Microsoft.Network/virtualNetworks/test-rg/subnets/test-subnet"
