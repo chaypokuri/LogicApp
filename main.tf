aaf128ba615e6b4d88cb149f5c79f7b6278bb8aa
chaypokuri <61907472+chaypokuri@users.noreply.github.com>
2024-09-16 18:51:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,5 @@
 provider "azurerm" {
   features {}
-  subscription_id = "c2bd123a-183f-43d5-bf41-c725494e595a"
-  tenant_id       = "3180c264-31bc-4113-8f50-b7393a40457b"
-  client_id       = "1a046c02-8c39-4f1d-b30b-93f41a9c6b15"
-  client_secret   = "kUz8Q~qwom0J-MM5ZNqexXyUOguygMj5QELdhdl5"
 }
 terraform {
   required_providers {
@@ -64,6 +60,5 @@ resource "azurerm_logic_app_standard" "this" {
   app_service_plan_id        = azurerm_app_service_plan.this.id
   storage_account_name       = azurerm_storage_account.this.name
   storage_account_access_key = azurerm_storage_account.this.primary_access_key
-  #virtual_network_subnet_id  = azurerm_subnet.this.id
 }
 
