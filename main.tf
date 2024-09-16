0a5f86413b0d9da138d2b7188efbdee344ef6161
chaypokuri <61907472+chaypokuri@users.noreply.github.com>
2024-09-16 10:05:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,15 +13,57 @@ terraform {
     }
   }
 }
+resource "azurerm_resource_group" "this" {
+  name     = "azure-test-rg"
+  location = "east us"
+}
 
-  resource "azurerm_logic_app_standard" "example" {
-  name                       = "test-azure-functions"
- resource_group_name = "test-rg"
-  location            = "east us"
-  storage_account_name       = "teststorageaccount001"
-  storage_account_access_key = "BjYCZEHX2OCqz/HWJdWp4rHfpLIAMJt0adLxzQnpItXCSyKwArG8iCgFfVNW/6pgabrtlniTcEaK+AStF5N8xQ=="
-  app_service_plan_id     = "/subscriptions/c2bd123a-183f-43d5-bf41-c725494e595a/resourceGroups/test-rg/providers/Microsoft.Web/serverfarms/test-asp"
-  #virtual_network_subnet_id = "/subscriptions/c2bd123a-183f-43d5-bf41-c725494e595a/resourceGroups/test-rg/providers/Microsoft.Network/virtualNetworks/test-rg/subnets/test-subnet"
-  site_config {  
+resource "azurerm_storage_account" "this" {
+  name                     = "logicsapptestsa"
+  resource_group_name      = azurerm_resource_group.this.name
+  location                 = azurerm_resource_group.this.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+resource "azurerm_virtual_network" "this" {
+  name                = "this-virtual-network"
+  address_space       = ["10.0.0.0/24"]
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+}
+
+resource "azurerm_subnet" "this" {
+  name                 = "this-subnet"
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = ["10.0.1.0/27"]
+  delegation {
+    name = "this-delegation"
+    service_delegation {
+      name    = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
   }
 }
+
+resource "azurerm_app_service_plan" "this" {
+  name                = "azure-functions-test-service-plan"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  kind                = "elastic"
+  sku {
+    tier = "WorkflowStandard"
+    size = "WS1"
+  }
+}
+
+resource "azurerm_logic_app_standard" "this" {
+  name                       = "test-azure-lapp"
+  location                   = azurerm_resource_group.this.location
+  resource_group_name        = azurerm_resource_group.this.name
+  app_service_plan_id        = azurerm_app_service_plan.this.id
+  storage_account_name       = azurerm_storage_account.this.name
+  storage_account_access_key = azurerm_storage_account.this.primary_access_key
+  virtual_network_subnet_id  = azurerm_subnet.this.id
+}
+
