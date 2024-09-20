a6e77ddbbfdd2ddda1d2d92bc368ca5ff5911235
chaypokuri <61907472+chaypokuri@users.noreply.github.com>
2024-09-20 16:16:42 +0530

    Updated main.tf for app gw

--- main.tf
+++ main.tf
@@ -38,17 +38,6 @@ resource "azurerm_public_ip" "example" {
   location            = azurerm_resource_group.example.location
   allocation_method   = "Static"
 }
-
-locals {
-  backend_address_pool_name      = "${azurerm_virtual_network.example.name}-beap"
-  frontend_port_name             = "${azurerm_virtual_network.example.name}-feport"
-  frontend_ip_configuration_name = "${azurerm_virtual_network.example.name}-feip"
-  http_setting_name              = "${azurerm_virtual_network.example.name}-be-htst"
-  listener_name                  = "${azurerm_virtual_network.example.name}-httplstn"
-  request_routing_rule_name      = "${azurerm_virtual_network.example.name}-rqrt"
-  redirect_configuration_name    = "${azurerm_virtual_network.example.name}-rdrcfg"
-}
-
 resource "azurerm_application_gateway" "network" {
   name                = "example-appgateway"
   resource_group_name = azurerm_resource_group.example.name
@@ -66,21 +55,21 @@ resource "azurerm_application_gateway" "network" {
   }
 
   frontend_port {
-    name = local.frontend_port_name
+    name = frontend_port_name
     port = 443 # HTTPS port
   }
 
   frontend_ip_configuration {
-    name                 = local.frontend_ip_configuration_name
+    name                 = frontend_ip_configuration_name
     public_ip_address_id = azurerm_public_ip.example.id
   }
 
   backend_address_pool {
-    name = local.backend_address_pool_name
+    name = backend_address_pool_name
   }
 
   backend_http_settings {
-    name                  = local.http_setting_name
+    name                  = http_setting_name
     cookie_based_affinity = "Disabled"
     path                  = "/path1/"
     port                  = 443 
@@ -89,9 +78,9 @@ resource "azurerm_application_gateway" "network" {
   }
 
   http_listener {
-    name                           = local.listener_name
-    frontend_ip_configuration_name = local.frontend_ip_configuration_name
-    frontend_port_name             = local.frontend_port_name
+    name                           = listener_name
+    frontend_ip_configuration_name = frontend_ip_configuration_name
+    frontend_port_name             = frontend_port_name
     protocol                       = "Https" 
   }
 
@@ -103,8 +92,8 @@ resource "azurerm_application_gateway" "network" {
     name                       = local.request_routing_rule_name
     priority                   = 9
     rule_type                  = "Basic"
-    http_listener_name         = local.listener_name
-    backend_address_pool_name  = local.backend_address_pool_name
-    backend_http_settings_name = local.http_setting_name
+    http_listener_name         = listener_name
+    backend_address_pool_name  = backend_address_pool_name
+    backend_http_settings_name = http_setting_name
   }
 }
