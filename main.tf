5729cae94fd214a8714844950a0af948ae5a80d7
chaypokuri <61907472+chaypokuri@users.noreply.github.com>
2024-09-24 19:36:55 +0530

    Update main.tf for fail mock test

--- main.tf
+++ main.tf
@@ -13,66 +13,98 @@ terraform {
     }
   }
 }
-resource "azurerm_resource_group" "rg" {
-  name     = "resource-group"
-  location = "Spain Central"
+resource "azurerm_resource_group" "this" {
+  name     = "this-resources"
+  location = "West Europe"
 }
 
-resource "azurerm_public_ip" "pip" {
-  name                = "pip"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku                 = "Standard"
+resource "azurerm_virtual_network" "this" {
+  name                = "this-network"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  address_space       = ["10.254.0.0/16"]
+}
+
+resource "azurerm_subnet" "this" {
+  name                 = "this"
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = ["10.254.0.0/24"]
+}
+
+resource "azurerm_public_ip" "this" {
+  name                = "this-pip"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
   allocation_method   = "Static"
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "virtual-network"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  address_space       = ["10.0.0.0/24"]
+# since these variables are re-used - a locals block makes this more maintainable
+locals {
+  backend_address_pool_name      = "${azurerm_virtual_network.this.name}-beap"
+  frontend_port_name             = "${azurerm_virtual_network.this.name}-feport"
+  frontend_ip_configuration_name = "${azurerm_virtual_network.this.name}-feip"
+  http_setting_name              = "${azurerm_virtual_network.this.name}-be-htst"
+  listener_name                  = "${azurerm_virtual_network.this.name}-httplstn"
+  request_routing_rule_name      = "${azurerm_virtual_network.this.name}-rqrt"
+  redirect_configuration_name    = "${azurerm_virtual_network.this.name}-rdrcfg"
 }
 
-resource "azurerm_subnet" "snet" {
-  name                 = "virtual-subnet"
-  resource_group_name  = azurerm_virtual_network.vnet.resource_group_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.0.0/24"]
-}
-resource "azurerm_application_gateway" "app_gateway" {
-  source              = "aztfm/application-gateway/azurerm"
-  version             = ">=2.0.0"
-  name                = "application-gateway"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  sku_name            = "WAF_v2"
-  capacity            = 1
-  subnet_id           = azurerm_subnet.snet.id
-  frontend_ip_configuration = {
-    public_ip_address_id = azurerm_public_ip.pip.id
+resource "azurerm_application_gateway" "network" {
+  name                = "this-appgateway"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+
+  sku {
+    name     = "Standard_v2"
+    tier     = "Standard_v2"
+    capacity = 2
   }
-  backend_address_pools {
-    name         = "backend-address-pool"
-    ip_addresses = ["10.0.0.4", "10.0.0.5"]
+
+  gateway_ip_configuration {
+    name      = "my-gateway-ip-configuration"
+    subnet_id = azurerm_subnet.this.id
   }
-  http_listeners {
-    name                      = "http-listener"
-    frontend_ip_configuration = "Public"
-    protocol                  = "Http"
-    port                      = 80
+
+  frontend_port {
+    name = local.frontend_port_name
+    port = 80
   }
+
+  frontend_ip_configuration {
+    name                 = local.frontend_ip_configuration_name
+    public_ip_address_id = azurerm_public_ip.this.id
+  }
+
+  backend_address_pool {
+    name = local.backend_address_pool_name
+  }
+
   backend_http_settings {
-    name     = "backend-http-setting-1"
-    protocol = "Http"
-    port     = 80
+    name                  = local.http_setting_name
+    cookie_based_affinity = "Disabled"
+    path                  = "/path1/"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 60
   }
-  request_routing_rules  {
-    name                       = "request-routing-rule"
-    priority                   = 100
-    http_listener_name         = "http-listener"
-    backend_address_pool_name  = "backend-address-pool"
-    backend_http_settings_name = "backend-http-setting"
+
+  http_listener {
+    name                           = local.listener_name
+    frontend_ip_configuration_name = local.frontend_ip_configuration_name
+    frontend_port_name             = local.frontend_port_name
+    protocol                       = "Http"
   }
+
+  request_routing_rule {
+    name                       = local.request_routing_rule_name
+    priority                   = 9
+    rule_type                  = "Basic"
+    http_listener_name         = local.listener_name
+    backend_address_pool_name  = local.backend_address_pool_name
+    backend_http_settings_name = local.http_setting_name
+  }
+
   ssl_policy {
    min_protocol_version = "TLSv1_3"
    disabled_protocols   = ["TLSv1_0", "TLSv1_1","TLSv1_2"]
