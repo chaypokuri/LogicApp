5b51750db8db77af6c811891d3fc58274004b30d
chaypokuri <61907472+chaypokuri@users.noreply.github.com>
2024-09-26 11:02:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,4 @@
-provider "azurerm" {
-  features {}
-  subscription_id = "c2bd123a-183f-43d5-bf41-c725494e595a"
-  tenant_id       = "3180c264-31bc-4113-8f50-b7393a40457b"
-  client_id       = "1a046c02-8c39-4f1d-b30b-93f41a9c6b15"
-  client_secret   = "kUz8Q~qwom0J-MM5ZNqexXyUOguygMj5QELdhdl5"
-}
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
+
 resource "azurerm_resource_group" "this" {
   name     = "this-resources"
   location = "West Europe"
